c12c1cca1b83a27b97520ee9a1ee878182e14a55
michaelderiss <76720388+michaelderiss@users.noreply.github.com>
2026-03-15 02:29:01 -0700

    Update what array with excuses-related objects or situations

--- excuse-generator.js
+++ excuse-generator.js
@@ -1,3 +1,28 @@
-const who = ['I', 'My friend', 'The dog', 'The cat']; // added new array of subjects
+'use strict';
 
-// existing code
+const who = ['I', 'My friend', 'The dog', 'The cat'];
+
+const what = [
+    'overslept',
+    'forgot my keys',
+    'had car trouble',
+    'got stuck in traffic',
+    'had a family emergency',
+    'missed the alarm',
+    'lost track of time'
+];
+
+const excuses = [
+    'I was late because I overslept.',
+    'I forgot my keys.',
+    'My car broke down.',
+    'I had an emergency.',
+    'I ran into traffic.',
+];
+
+function getRandomExcuse() {
+    const randomIndex = Math.floor(Math.random() * excuses.length);
+    return excuses[randomIndex];
+}
+
+console.log(getRandomExcuse());

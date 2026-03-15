4cd14976832080a8e35c82410c3df33c1434d63a
michaelderiss <76720388+michaelderiss@users.noreply.github.com>
2026-03-15 02:34:02 -0700

    Update excuse-generator.js with new content

--- excuse-generator.js
+++ excuse-generator.js
@@ -1,4 +1,28 @@
-const actions = [
+'use strict';
+
+const who = ['I', 'My friend', 'The dog', 'The cat'];
+
+const what = [
+    'overslept',
+    'forgot my keys',
+    'had car trouble',
+    'got stuck in traffic',
+    'had a family emergency',
+    'missed the alarm',
+    'lost track of time'
+];
+
+const when = [
+    'today',
+    'yesterday',
+    'this morning',
+    'last night',
+    'earlier',
+    'a moment ago',
+    'this afternoon'
+];
+
+const action = [
     'was delayed by',
     'was held up by',
     'was stopped by',
@@ -8,4 +32,29 @@ const actions = [
     'was slowed down by'
 ];
 
-module.exports = actions;
+// Get random value from the who array
+const randomWho = who[Math.floor(Math.random() * who.length)];
+
+// Get random value from the action array
+const randomAction = action[Math.floor(Math.random() * action.length)];
+
+// Get random value from the what array
+const randomWhat = what[Math.floor(Math.random() * what.length)];
+
+// Get random value from the when array
+const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+const excuses = [
+    'I was late because I overslept.',
+    'I forgot my keys.',
+    'My car broke down.',
+    'I had an emergency.',
+    'I ran into traffic.',
+];
+
+function getRandomExcuse() {
+    const randomIndex = Math.floor(Math.random() * excuses.length);
+    return excuses[randomIndex];
+}
+
+console.log(getRandomExcuse());

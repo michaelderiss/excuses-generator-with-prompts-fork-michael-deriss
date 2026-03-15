a088a54e33a380bde8b64d4ddfaf6b6a46780582
michaelderiss <76720388+michaelderiss@users.noreply.github.com>
2026-03-15 02:39:35 -0700

    Update excuse-generator.js
    Corrected Claude's mistake.

--- excuse-generator.js
+++ excuse-generator.js
@@ -1,1 +1,50 @@
-const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+'use strict';
+
+const who = ['I', 'My friend', 'The dog', 'The cat'];
+
+const what = [
+    'overslept',
+    'forgot my keys',
+    'had car trouble',
+    'got stuck in traffic',
+    'had a family emergency',
+    'missed the alarm',
+    'lost track of time'
+];
+
+const when = [
+    'today',
+    'yesterday',
+    'this morning',
+    'last night',
+    'earlier',
+    'a moment ago',
+    'this afternoon'
+];
+
+const action = [
+    'was delayed by',
+    'was held up by',
+    'was stopped by',
+    'was interrupted by',
+    'was caught in',
+    'was stuck because of',
+    'was slowed down by'
+];
+
+// Get random value from the who array
+const randomWho = who[Math.floor(Math.random() * who.length)];
+
+// Get random value from the action array
+const randomAction = action[Math.floor(Math.random() * action.length)];
+
+// Get random value from the what array
+const randomWhat = what[Math.floor(Math.random() * what.length)];
+
+// Get random value from the when array
+const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+// Build the excuse sentence
+const excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + ".";
+
+console.log(excuse);

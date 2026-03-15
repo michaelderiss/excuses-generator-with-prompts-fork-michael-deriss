a91e67664a34bc81cd0ce4a4568eb1b46e996063
michaelderiss <76720388+michaelderiss@users.noreply.github.com>
2026-03-15 02:27:55 -0700

    Added a new array called who with possible subjects for excuses

--- excuse-generator.js
+++ excuse-generator.js
@@ -1,16 +1,3 @@
-'use strict';
+const who = ['I', 'My friend', 'The dog', 'The cat']; // added new array of subjects
 
-const excuses = [
-    'I was late because I overslept.',
-    'I forgot my keys.',
-    'My car broke down.',
-    'I had an emergency.',
-    'I ran into traffic.',
-];
-
-function getRandomExcuse() {
-    const randomIndex = Math.floor(Math.random() * excuses.length);
-    return excuses[randomIndex];
-}
-
-console.log(getRandomExcuse());
+// existing code
